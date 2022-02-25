72a03326b0cc00140aa8f64cc1253f60e2718e5f
PRAPHIL <praveenmatthewphilip@gmail.com>
2022-02-26 03:49:56 +0530

    fixed flow bite dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 module.exports = {
   content: [
-    "./node_modules/flowbite/**/*.js",
     "./src/**/*.{html,ts}"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
   ],
 }
