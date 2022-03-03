c8c0766278363d93b1cbfc43ad9695c15247382e
PRAPHIL <praveenmatthewphilip@gmail.com>
2022-03-03 22:41:44 +0530

    removing unsed dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-
-import { OrderListModule } from 'primeng/orderlist';
-import { ButtonModule } from 'primeng/button';
-import { TableModule } from 'primeng/table';
-
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 import { ProductService } from './productservice.service';
-
 import { ProductsModule } from './products/products.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { ConfirmationService, MessageService } from 'primeng/api';
-import { RippleModule } from 'primeng/ripple';
-import { SignInComponent } from './common/sign-in/sign-in.component';
 import { CommonPartsModule } from './common/commonparts.module';
 import { NotfoundComponent } from './notfound/notfound.component';
 
@@ -27,17 +15,11 @@ import { NotfoundComponent } from './notfound/notfound.component';
   declarations: [AppComponent, NotfoundComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    FontAwesomeModule,
     HttpClientModule,
-    OrderListModule,
     BrowserAnimationsModule,
-    ButtonModule,
-    TableModule,
+    AppRoutingModule,
     ProductsModule,
     DashboardModule,
-    RippleModule,
     CommonPartsModule,
   ],
   providers: [ProductService, MessageService, ConfirmationService],
